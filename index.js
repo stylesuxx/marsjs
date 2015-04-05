b109bfe0da191fbf8e170ce091c1da769d4b17ce
stylesuxx <stylesuxx@gmail.com>
2015-04-05 22:02:41 +0200

    Added functinality for start button. Added functionality to update single cells of the field

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
+var field = new Field(8000);
+
+$('button.start-simulation').click(function(e) {
+  e.preventDefault();
+
+  var drawField = function(cells) {
+    var container = "";
+    for(var i = 0; i < cells.length; i++) {
+      var cell = cells[i];
+      var color = ''
+      if(user = cell.getLastUser()) {
+        color = user.getColor();
+        color = 'style="background-color: ' + color + ';"';
+      }
+      var action = cell.getLastAction();
+      var title = i + ': ' + cell.getInstruction().toString();
+      var item = '<div index="' + i + '" ' + color + ' class="cell" action="' + action + '" title="' + title + '"></div>';
+      container += item;
+    }
+    $('.row.field').html(container);
+  };
+
+  var fieldUpdate = function(touched) {
+    var cells = field.getField();
+    for(var i = 0; i < touched.length; i++) {
+      var index = touched[i];
+      var cell = cells[index];
+      var action = cell.getLastAction();
+      var color = 'grey';
+      if(cell.getLastUser()) {
+        color = cell.getLastUser().getColor();
+      }
+      var title = i + ': ' + cell.getInstruction().toString();
+      $('.field .cell[index=' + index + ']')
+        .attr('action', action)
+        .attr('title', title)
+        .css('background-color', color);
+    };
+    setTimeout(function() {
+      field.move();
+    }, 50);
+  }
+
+  var cells = field.getField();
+  drawField(cells);
+
+  field.start(fieldUpdate);
+});
 
 $('button.load-warrior').click(function(e) {
   e.preventDefault();
@@ -22,4 +70,6 @@ $('button.load-warrior').click(function(e) {
       '<td>' + current.getB().getMode().toUpperCase() + '</td>' +
       '<td>' + current.getB().getValue() + '</td></tr>');
   }
+
+  field.addWarrior(warrior);
 });
