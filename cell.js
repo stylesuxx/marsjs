45ca3273702fb57a28be3e40b951e378b3a396b9
stylesuxx <stylesuxx@gmail.com>
2015-04-06 16:49:52 +0200

    Setters before getters, added ;

--- cell.js
+++ cell.js
@@ -4,28 +4,28 @@ var Cell = function(instruction) {
   // Last action may be: init | read | write
   this.lastAction = 'init';
   this.lastUser = null;
-}
-
-Cell.prototype.getInstruction = function() {
-  return this.instruction;
-}
+};
 
 Cell.prototype.setInstruction = function(instruction) {
   this.instruction = instruction;
 };
 
-Cell.prototype.getLastAction = function() {
-  return this.lastAction;
-}
+Cell.prototype.getInstruction = function() {
+  return this.instruction;
+};
 
 Cell.prototype.setLastAction = function(action) {
   this.lastAction = action;
-}
+};
 
-Cell.prototype.getLastUser = function() {
-  return this.lastUser;
-}
+Cell.prototype.getLastAction = function() {
+  return this.lastAction;
+};
 
 Cell.prototype.setLastUser = function(warrior) {
   this.lastUser = warrior;
-}
+};
+
+Cell.prototype.getLastUser = function() {
+  return this.lastUser;
+};
