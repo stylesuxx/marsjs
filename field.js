9d5690bcd92a1731eb31b079e2402320c2f6797f
stylesuxx <stylesuxx@gmail.com>
2015-04-06 14:26:49 +0200

    Refactored jmz functionality to use A and B number helpers

--- field.js
+++ field.js
@@ -1180,7 +1180,7 @@ var Field = function(coreSize) {
       case "a":
       case "ba": {
         var address = pc +1;
-        var a_nr = this.field[b_adr].getInstruction().getA().getValue();
+        var a_nr = this.getANumber(b_adr);
         if(a_nr == 0) {
           address = a_adr;
         }
@@ -1196,7 +1196,7 @@ var Field = function(coreSize) {
       case "b":
       case "ab": {
         var address = pc + 1;
-        var b_nr = this.field[b_adr].getInstruction().getB().getValue();
+        var b_nr = this.getBNumber(b_adr);
         if(b_nr == 0) {
           address = a_adr;
         }
@@ -1213,8 +1213,8 @@ var Field = function(coreSize) {
       case "f":
       case "i": {
         var address = pc + 1;
-        var a_nr = this.field[b_adr].getInstruction().getA().getValue();
-        var b_nr = this.field[b_adr].getInstruction().getB().getValue();
+        var a_nr = this.getANumber(b_adr);
+        var b_nr = this.getBNumber(b_adr);
         if(a_nr == 0 && b_nr == 0) {
           address = a_adr;
         }
