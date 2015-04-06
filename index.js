a4a67a1183b756c13bc9251cfd737668f369e742
stylesuxx <stylesuxx@gmail.com>
2015-04-06 21:18:30 +0200

    Refresh field after a warrior has been added

--- index.js
+++ index.js
@@ -1,7 +1,67 @@
-var field = new Field(8000);
+var field = new Field(8000, 20000);
 
 $('button.start-simulation').click(function(e) {
   e.preventDefault();
+
+  var fieldUpdate = function(touched, callback) {
+    var cells = field.getField();
+    for(var i = 0; i < touched.length; i++) {
+      var index = touched[i];
+      var cell = cells[index];
+      var action = cell.getLastAction();
+      var color = 'grey';
+      if(cell.getLastUser()) {
+        color = cell.getLastUser().getColor();
+      }
+      var title = index + ': ' + cell.getInstruction().toString();
+      $('.field .cell[index=' + index + ']')
+        .attr('action', action)
+        .attr('title', title)
+        .css('background-color', color);
+    };
+
+    // Give the screen some time to update and execute the callback
+    setTimeout(function() {
+      callback();
+    }, 0);
+  }
+
+  field.start(fieldUpdate);
+  //field.start();
+});
+
+$('button.load-warrior').click(function(e) {
+  e.preventDefault();
+  var text = $('textarea.warrior').val().split('\n');
+  var parser = new Parser(text);
+  var warrior = parser.getWarrior();
+
+  var count = $(".warriors .warrior").length + 1;
+
+  $(".warrior-template")
+    .clone()
+    .appendTo(".warriors")
+    .removeClass("warrior-template")
+    .removeClass("hidden")
+    .addClass("warrior")
+    .addClass("warrior-" + count);
+
+  $(".warrior-" + count + " .warrior-info").append().html('Program <strong>' + warrior.getName() + '</strong> (length ' + warrior.getLength() + ') by <strong>' + warrior.getAuthor() + '</strong>');
+
+  for(var i = 0, code = warrior.getCode(); i < code.length; i++) {
+    var current = code[i];
+    var start = '';
+    if(i == warrior.getStart()) {
+      start = 'START';
+    }
+    $(".warrior-" + count + " table tbody").append('<tr>' +
+      '<td>' + start + '</td>' +
+      '<td>' + current.getOpcode().toUpperCase() + '.' + current.getModifier().toUpperCase() + '</td>'+
+      '<td>' + current.getA().getMode().toUpperCase() + '</td>' +
+      '<td>' + current.getA().getValue() + '</td>' +
+      '<td>' + current.getB().getMode().toUpperCase() + '</td>' +
+      '<td>' + current.getB().getValue() + '</td></tr>');
+  }
 
   var drawField = function(cells) {
     var container = "";
@@ -20,56 +80,8 @@ $('button.start-simulation').click(function(e) {
     $('.row.field').html(container);
   };
 
-  var fieldUpdate = function(touched) {
-    var cells = field.getField();
-    for(var i = 0; i < touched.length; i++) {
-      var index = touched[i];
-      var cell = cells[index];
-      var action = cell.getLastAction();
-      var color = 'grey';
-      if(cell.getLastUser()) {
-        color = cell.getLastUser().getColor();
-      }
-      var title = i + ': ' + cell.getInstruction().toString();
-      $('.field .cell[index=' + index + ']')
-        .attr('action', action)
-        .attr('title', title)
-        .css('background-color', color);
-    };
-    setTimeout(function() {
-      field.move();
-    }, 50);
-  }
+  field.addWarrior(warrior);
 
   var cells = field.getField();
   drawField(cells);
-
-  field.start(fieldUpdate);
-});
-
-$('button.load-warrior').click(function(e) {
-  e.preventDefault();
-  var text = $('textarea.warrior').val().split('\n');
-  var parser = new Parser(text);
-  var warrior = parser.getWarrior();
-
-  $('.warrior-info').html('Program <strong>' + warrior.getName() + '</strong> (length ' + warrior.getLength() + ') by <strong>' + warrior.getAuthor() + '</strong>');
-  $('table.warrior tbody').html('');
-
-  for(var i = 0, code = warrior.getCode(); i < code.length; i++) {
-    var current = code[i];
-    var start = '';
-    if(i == warrior.getStart()) {
-      start = 'START';
-    }
-    $('table.warrior tbody').append('<tr>' +
-      '<td>' + start + '</td>' +
-      '<td>' + current.getOpcode().toUpperCase() + '.' + current.getModifier().toUpperCase() + '</td>'+
-      '<td>' + current.getA().getMode().toUpperCase() + '</td>' +
-      '<td>' + current.getA().getValue() + '</td>' +
-      '<td>' + current.getB().getMode().toUpperCase() + '</td>' +
-      '<td>' + current.getB().getValue() + '</td></tr>');
-  }
-
-  field.addWarrior(warrior);
 });
